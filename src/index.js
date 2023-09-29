02b0964079a72663fb9ead1f5ff171f50e610219
Rowland <rowland.wanderi@student.moringaschool.com>
2023-09-29 20:06:19 +0300

    complete assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
+
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener("submit",(e) => {
+        e.preventDefault()
+        const enterId = document.querySelector('input#searchByID');
+
+        fetch(`http://localhost:3000/movies/${enterId.value}`)
+        .then(res => (res.json()))
+        .then( ourArrayData => { 
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+
+            title.innerText = ourArrayData .title;
+            summary.innerText = ourArrayData .summary;
+        })
+        
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
